a0ad92e4c024ccd9592037315f24871280fb64fa
b3nj5m1n <b3nj4m1n@gmx.net>
2023-04-22 16:08:55 +0200

    test

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    fenix,
+    crane,
+  }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          giddy =
+            let
+              craneLib = crane.lib.${system}.overrideToolchain
+                fenix.packages.${system}.minimal.toolchain;
+            in
+              craneLib.buildPackage {
+                src = ./.;
+              };
+        };
+        defaultPackage = packages.giddy;
+        apps = {
+          giddy = flake-utils.lib.mkApp { drv = packages.giddy; };
+        };
+        defaultApp = apps.giddy;
+      }
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,34 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
